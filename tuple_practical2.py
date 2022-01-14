94c4a128a2caeead9961d4459d5b397f96f48152
Chhabhaiya Devanshi <96674544+ChhabhaiyaDevanshi@users.noreply.github.com>
2022-01-14 21:12:56 +0530

    Update tuple_practical2.py

--- tuple_practical2.py
+++ tuple_practical2.py
@@ -1,31 +1,32 @@
-# # 20CE011-CHHABHAIYA DEVANSHI
+# https://github.com/ChhabhaiyaDevanshi/python_pra2/blob/main/tuple_practical2.py
+# 20CE011-CHHABHAIYA DEVANSHI
 
 # ::tuples::
 
 # ---------------------->que-1: Write a Python program to create a tuple with different data types.<--------------------------
-# mytuple = ("name", 30, True, "city")
-# print(mytuple, type(mytuple))
+# mytuple = ("name", 30, True, "city") #create tuple with diff data types
+# print(mytuple, type(mytuple)) 
 
 # ---------------------->que-2: Write a Python program to create a tuple with numbers and print one item.<--------------------------
 
 # tuple = (12, 4.5, 25)
-# print(tuple[1])
+# print(tuple[1]) #print item at index 1
 
 # ---------------------->que-3: Write a Python program to add an item in a tuple.<--------------------------
 
 # thistuple = ("kiwi", "apple", "orange")
 # print(thistuple)
-# x = list(thistuple)
-# x.append("banana")
-# thistuple = tuple(x)
+# x = list(thistuple) #convert tuple into list
+# x.append("banana") #add item using append()
+# thistuple = tuple(x) #convert list int tuple
 # print(thistuple)
 
 # ---------------------->que-4: Write a Python program to convert a tuple to a string.<--------------------------
 
-# def coverttostring(tup):
+# def coverttostring(tup):  #fun to convert a tuple into string
 #     str = ' '
 #     for item in tup:
-#         str = str+item
+#         str = str+item  #add items one by one into str variable 
 #     return str
 
 
@@ -36,4 +37,4 @@
 # ---------------------->que-5: Write a Python program to find the length of a tuple.<--------------------------
 
 # thistuple = ("apple", "name", 12, 5.6)
-# print(len(thistuple))
+# print(len(thistuple)) #len() to find length of tuple
