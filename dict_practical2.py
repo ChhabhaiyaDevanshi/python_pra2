655c7cb96d2e82acc55a0fc19fd18ead201ee99a
Chhabhaiya Devanshi <96674544+ChhabhaiyaDevanshi@users.noreply.github.com>
2022-01-14 21:07:02 +0530

    Update dict_practical2.py

--- dict_practical2.py
+++ dict_practical2.py
@@ -1,3 +1,4 @@
+# https://github.com/ChhabhaiyaDevanshi/python_pra2/blob/main/dict_practical2.py
 # 20CE011-CHHABHAIYA DEVANSHI
 
 # ::dictionary::
@@ -17,9 +18,9 @@
 # ---------------------->que-1: Write a Python script to check whether a given key already exists in a dictionary.<--------------------------
 
 # print("the given dictionary is: ", d1)
-# val = input("enter the key: ")
+# val = input("enter the key: ") #take input from user
 
-# if val in d1:
+# if val in d1: #if the value is present in d1 then print else print not present
 #     print(val, "is present in dictionary.")
 # else:
 #     print(val, "is not present in dictionary.")
@@ -31,17 +32,16 @@
 
 # --------->method-2<-------------
 
-# d3 = d2.copy()
-# d3.update(d1)
+# d3 = d2.copy() #copy items from d2 to d3
+# d3.update(d1) #update all elements of d1 into d3 and print d3
 # print(d3)
 
 # ---------------------->que-3: Write a Python program to sum all the items in a dictionary.<--------------------------
 
-# def returnsum(x):
+# def returnsum(x): #function to sum all items 
 #     sum = 0
 #     for i in x:
 #         sum = sum+x[i]
-
 #     return sum
 
 
@@ -51,7 +51,7 @@
 #     "a3": 50,
 # }
 # print("given dictionary is:\n", x)
-# print("sum:", returnsum(x))
+# print("sum:", returnsum(x))  #call returnsum() fun
 
 # ---------------------->que-4: Write a Python script to add a key to a dictionary.<--------------------------
 
@@ -62,7 +62,7 @@
 # }
 
 # print("given dictionary is:\n", A1)
-# A1.update({"v4": "pink"})
+# A1.update({"v4": "pink"}) #add element using update()
 # print("updated dictionary is:\n", A1)
 
 # ---------------------->que-5: Write a Python script to concatenate the following dictionaries to create a new one.<--------------------------
